28012121d8e01b3d51b5dda533d5c38e42630170
Sampat Karehonna <sampatkarehonna@gmail.com>
2025-05-15 23:29:23 +0530

    User input

---
+++ UserInput.py
@@ -0,0 +1,8 @@
+#By default string input take data as a string we need explicitly typecasting it.
+
+name = input("What is your name?")
+age = int(input("How old are you?"))
+
+age = age +1;
+print(name.capitalize())
+print(name +" "+"hello"+ "your age is "+str(age))
