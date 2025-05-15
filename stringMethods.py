74c46a68607a962629fbde1f3ed8427406b8a06f
Sampat Karehonna <sampatkarehonna@gmail.com>
2025-05-15 23:12:33 +0530

    String methods

---
+++ stringMethods.py
@@ -0,0 +1,13 @@
+#Here demonstrating the string methods.
+
+name = "hello"
+print(len(name))
+print(name.find("a"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("l"))
+print(name.replace("l","m"))
+print(name*2)
