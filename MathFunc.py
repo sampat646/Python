72e22e519ccf5e4bd7b5860f71cbff22ff597f0d
Sampat Karehonna <sampatkarehonna@gmail.com>
2025-05-16 22:06:57 +0530

    Math functions

---
+++ MathFunc.py
@@ -0,0 +1,14 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi))#3
+print(math.ceil(pi))#4
+print(math.floor(pi))#3
+print(math.pow(pi,2))#9.86
+print(math.sqrt(49))#7.0
+print(max(x,y,z))
+print(min(x,y,z))
